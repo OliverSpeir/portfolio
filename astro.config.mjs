5d9da8fc8ae28215e5cc9fef83242911975f01bf
Oliver Speir <oliverspeir9@gmail.com>
2023-04-10 17:59:48 -0600

    acp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vercelStatic from '@astrojs/vercel/static';
-export default defineConfig ({ output:'server', adapter:vercelStatic(), });
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: vercel()
+});
