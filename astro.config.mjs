514703c2db1301efb7f686a305deefdc3d41743e
Oliver Speir <oliverspeir9@gmail.com>
2023-04-17 01:07:58 -0600

    acp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from "astro/config";
 import vercelStatic from "@astrojs/vercel/static";
-// import partytown from "@astrojs/partytown";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   output: "static",
   adapter: vercelStatic(),
-  // integrations: [
-  //   partytown({
-  //     config: {
-  //       forward: ["dataLayer.push"],
-  //     },
-  //   }),
-  // ],
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
