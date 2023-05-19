4e454c11fcd22d8f3a9876b2a34681680e68aa7a
Oliver Speir <oliverspeir9@gmail.com>
2023-05-18 18:39:00 -0600

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import vercelStatic from "@astrojs/vercel/static";
 import partytown from "@astrojs/partytown";
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: vercelStatic(),
+  site: 'https://www.oliverspeir.com',
   integrations: [partytown({
     config: {
       forward: ["dataLayer.push"]
     }
-  })]
+  }), sitemap()]
 });
