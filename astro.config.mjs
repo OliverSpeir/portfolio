67b5b2a14fb1d1921888e404f7396ddefc4a2e75
Oliver Speir <oliverspeir9@gmail.com>
2023-09-13 02:59:12 -0600

    added anchor links to blog headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import { accessibleCheckbox } from './src/rehype-plugins/addAccessability';
 import { removeInlineStyles } from './src/rehype-plugins/removeInlineStyles';
+import { autolinkConfig } from './src/rehype-plugins/rehype-autolink-config'
 import tailwind from "@astrojs/tailwind";
 import critters from "astro-critters";
 import purgecss from "astro-purgecss";
+import autolinkHeadings from 'rehype-autolink-headings'
+import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,7 +18,7 @@ export default defineConfig({
   adapter: vercelStatic(),
   site: 'https://www.oliverspeir.com',
   markdown: {
-    rehypePlugins: [accessibleCheckbox, removeInlineStyles]
+    rehypePlugins: [rehypeSlug, accessibleCheckbox, removeInlineStyles, [autolinkHeadings, autolinkConfig]]
   },
   integrations: [partytown({
     config: {
